5342ebcc3d0bbe31f5fa35efc8fd59bf2ae948c9
Fluorine <fluorinecypher@gmail.com>
2024-12-07 21:52:14 +0800

    main

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,18 @@ export const profileConfig: ProfileConfig = {
   name: 'Fluorine',
   bio: '爱折腾的萌新小白一枚UwU',
   links: [
-    
+    {
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://twitter.com',
+    },
+    {
+      name: 'Steam',
+      icon: 'fa6-brands:steam',
+      url: 'https://store.steampowered.com',
+    },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
